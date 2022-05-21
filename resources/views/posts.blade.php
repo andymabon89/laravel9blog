9f3697fd63cb2b3b2c8ec93246e7ad948b68e18c
andrewmabon <andrew.mabon@visualsoft.co.uk>
2022-05-21 20:02:39 +0100

    dynamic posts page achieved

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,13 @@
-@extends('layout')
+<x-layout>
+    @include('_posts-header')
 
-@section('content')
-        @foreach ($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {!! $post->title !!}
-                    </a>
-                </h1>
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    @if($posts->count())
+        <x-posts-grid :posts="$posts" />
+    @else
+        <p class="text-center">No posts here yet, please return later!</p>
+    @endif
 
-                <p>
-                    By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-                </p>
+</main>
 
-                <div>
-                    {!! $post->excerpt !!}
-                </div>
-            </article>
-        @endforeach
-@endsection
+</x-layout>
